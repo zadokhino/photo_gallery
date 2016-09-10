b6f81e51178ca1427fe6b0e445e489845063d963
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-10 13:50:33 +0600

    Created some actions and views for Users and Pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,33 @@
 class PicturesController < ApplicationController
 	def index
-		@pictures = Picture.all
+		@pictures = Picture.all.order('created_at DESC')
+	end
+
+	def show
+		@picture = Picture.find(params[:id])
+	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		@picture = current_user.pictures.build(picture_params)
+		if @picture.save
+			redirect_to user_path(current_user)
+		else
+			flash[:error] = "Not added"
+			redirect_to user_path(current_user)
+		end
+	end
+
+	def destroy
+		@picture = Picture.destroy(params[:id])
+		redirect_to user_path(current_user)
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:title, :image)
 	end
 end
