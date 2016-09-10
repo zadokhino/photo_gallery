6538ad1c4ceefab7bf9dd2daa19732f7621ebad9
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-10 12:07:06 +0600

    Created model Picture

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  	before_action :configure_permitted_parameters, if: :devise_controller?
   	protect_from_forgery with: :exception
 	def access_denied(exception)
 		flash[:danger] = exception.message
 		redirect_to root_url
+	end
+
+	private
+  
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
 	end
 end
