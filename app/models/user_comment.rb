054aa68d496ded1829931443ff8e5034047132ba
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-10 16:41:11 +0600

    Added seeds

--- app/models/user_comment.rb
+++ app/models/user_comment.rb
@@ -3,6 +3,6 @@ class UserComment < ActiveRecord::Base
 	belongs_to :picture
 
 	validates :text, presence: true, length: {maximum: 250}
-	validates :score, presence: true, numericality: true, 
+	validates :score, presence: true,
 			   numericality:{ greater_than_or_equal_to: 1, less_than_or_equal_to: 5 } 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+fixtures_path = Rails.root.join('app', 'assets', 'images', 'fixtures')
+User.create(name: "Admin", email:"admin@example.com", password: "123456", admin: true)
+User.create(name: "Oleg", email:"oleg@example.com", password: "123456")
+
+Picture.create(title: "some title", image: File.new(fixtures_path.join('1.jpg')), user_id: 1)
+Picture.create(title: "some title", image: File.new(fixtures_path.join('2.jpg')), user_id: 1)
+Picture.create(title: "some title", image: File.new(fixtures_path.join('3.jpg')), user_id: 1)
+Picture.create(title: "some title", image: File.new(fixtures_path.join('4.jpg')), user_id: 1)
+Picture.create(title: "some title", image: File.new(fixtures_path.join('5.jpg')), user_id: 1)
+Picture.create(title: "TITLE", image: File.new(fixtures_path.join('6.jpg')), user_id: 2)
+Picture.create(title: "TITLE", image: File.new(fixtures_path.join('7.jpg')), user_id: 2)
+Picture.create(title: "TITLE", image: File.new(fixtures_path.join('8.jpg')), user_id: 2)
+Picture.create(title: "TITLE", image: File.new(fixtures_path.join('9.jpg')), user_id: 2)
+Picture.create(title: "TITLE", image: File.new(fixtures_path.join('10.jpg')), user_id: 2)
