b6977c6c1874884ebde8501505c47ae20dcddb49
Oleg Zadokhin <zadokhino@gmail.com>
2016-09-10 13:20:44 +0600

    Destroy controller User and created conroller Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   resources :pictures
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
